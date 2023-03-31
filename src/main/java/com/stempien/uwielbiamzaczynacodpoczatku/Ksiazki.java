4bddb4b6f5933cc266a4d8f783ac959769812856
Adik08 <adik08.eamil@gmail.com>
2023-03-31 15:09:33 +0200

    miss click

---
+++ src/main/java/com/stempien/uwielbiamzaczynacodpoczatku/Ksiazki.java
@@ -0,0 +1,12 @@
+package com.stempien.uwielbiamzaczynacodpoczatku;
+
+public class Ksiazki {
+    public Ksiazki(String isbnOfBook,String autorOfBook,String titleOfBook,String yearOfBook,String descriptionOfBook,String publisherofBook) {
+        String isbn = isbnOfBook;
+        String autor = autorOfBook;
+        String title = titleOfBook;
+        String publisher = publisherofBook;
+        String year = yearOfBook;
+        String description = descriptionOfBook;
+    }
+}
